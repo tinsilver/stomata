daf9120764f0f6f6cf70b3b090292cbf32dc3df7
Badrinath Janakiraman <Thoughtworker@arvo-2.local>
2013-08-28 15:59:10 -0700

    Remove both validation & test for it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ActiveRecord::Base
-  validates :content, length: { maximum: 255 }
   attr_accessible :content, :title
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -8,9 +8,4 @@ class PostTest < ActiveSupport::TestCase
     assert_equal 'trivial', p.content
   end
 
-  test "titles greater than thirty long should be invalid" do
-    p = Post.create(:content => 'trivial', :content => "a" * 256)
-    assert_match /Content is too long/, p.errors.full_messages.join
-  end
-
 end
